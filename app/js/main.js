8fff73da7e72b126aa0ffe89f9866bac04cf9629
Ihustat <ger26dan@gmail.com>
2021-03-05 11:18:34 +0300

    added header, expert, age, why, slider sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
 $(function(){
-
+    $('.services__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/prev-arrow.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/next-arrow.png" alt=""></button>'
+      });
 
 });
